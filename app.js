0fcc3297c30f470eed9be6833efd021737b3e4ca
Mac <mac@abctech-thailand.com>
2015-09-02 08:58:49 +0700

    Init project with example code

---
+++ app.js
@@ -0,0 +1,18 @@
+var Slack = require('slack-client');
+
+var token = '';
+var autoReconnect = true;
+var autoMark = true;
+var slack = new Slack(token, autoReconnect, autoMark);
+
+slack.on('open', function() {
+  var channels = slack.channels;
+});
+
+slack.on('message', function(message) {
+  console.log(message);
+  var user = slack.getUserByID(message.user);
+  console.log(user.name + " : " + message.text);
+});
+
+slack.login();
